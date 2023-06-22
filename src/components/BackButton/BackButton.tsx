f3ced5492c0e0fe7f094591a9a230d935e6f11b4
Sergey Kazarezov <58355478+Serjino@users.noreply.github.com>
2023-06-22 10:49:44 +0300

    issue with backbtn fixed

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -2,12 +2,16 @@
 import Image from "next/image"
 import styles from './styles.module.css'
 import arrowBack from '../../static/svg/arrowBack.svg'
+import { useRouter } from 'next/navigation'
 
 export function BackBtn() {
+
+    const router = useRouter()
+
     return (
         <button
             className={styles.backButton}
-            onClick={(e) => history.go(-1)}
+            onClick={(e) => router?.back()}
         >
             <Image src={arrowBack} alt={'back'} width={24} />
             Назад
