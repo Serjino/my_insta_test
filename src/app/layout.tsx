f4efd93d33b82f28afa76d4f93a495f729b25382
Sergey Kazarezov <58355478+Serjino@users.noreply.github.com>
2023-06-22 10:04:39 +0300

    some meta for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"></meta>
       <body className={inter.className}>{children}</body>
     </html>
   )
